0d6b29e25089ead7f6ce15eb7cc656589e861dc9
KingShivamX <shivamhippalgave@gmail.com>
2025-02-08 00:12:34 +0530

    yes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import ProfileSection from "./sections/ProfileSection/ProfileSection"
 import ConfessionWall from "./sections/ConfessionWall/ConfessionWall"
 import Challenges from "./sections/Challenges/Challenges"
 import Quiz from "./sections/Challenges/quiz"
+import React from "react"
 import "./App.css"
 
 function App() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,18 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./App.css"
+import Hero from "./sections/DASHBOARD/Hero.jsx"
+import ConfessionWall from "./sections/ConfessionWall/ConfessionWall.jsx"
+import Challenges from "./sections/Challenges/Challenges.jsx"
+import ProfileSection from "./sections/ProfileSection/ProfileSection.jsx"
+import Quiz from "./sections/Challenges/quiz.jsx"
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+             <Hero />
+            <ProfileSection isMatched={false} />
+            <ConfessionWall />
+            <Challenges />
+            <Quiz />
     </StrictMode>
 )
