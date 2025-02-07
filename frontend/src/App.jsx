574976a4457218e4d81e77ea7f35327dc3924014
KingShivamX <shivamhippalgave@gmail.com>
2025-02-07 20:34:10 +0530

    count down

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import MainLayout from './layouts/MainLayout/MainLayout';
-import Hero from './components/Hero/Hero';
-import ProfileSection from './sections/ProfileSection/ProfileSection';
-import ConfessionWall from './sections/ConfessionWall/ConfessionWall';
-import Challenges from './sections/Challenges/Challenges';
-import ChallengesTest from './sections/Challenges/ChallengesTest';
-import Quiz from './sections/Challenges/quiz';
-import './App.css'
+import MainLayout from "./layouts/MainLayout/MainLayout"
+import Hero from "./components/Hero/Hero"
+import ProfileSection from "./sections/ProfileSection/ProfileSection"
+import ConfessionWall from "./sections/ConfessionWall/ConfessionWall"
+import Challenges from "./sections/Challenges/Challenges"
+import ChallengesTest from "./sections/Challenges/ChallengesTest"
+import Quiz from "./sections/Challenges/quiz"
+import "./App.css"
 
 function App() {
-  return (
-    <MainLayout>
-      <Hero />
-      <ProfileSection isMatched={false} />
-      <ConfessionWall />
-      <Challenges />
-      <Quiz />
-      <ChallengesTest />
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <Hero />
+            <ProfileSection isMatched={false} />
+            <ConfessionWall />
+            <Challenges />
+            <Quiz />
+            <ChallengesTest />
+        </MainLayout>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Hero from './sections/DASHBOARD/Hero.jsx'
-import Challenges from './sections/Challenges/Challenges.jsx'
-import ConfessionWall from './sections/ConfessionWall/ConfessionWall.jsx'
-import ProfileSection from './sections/ProfileSection/ProfileSection.jsx'
-import Quiz from './sections/Challenges/quiz.jsx'
-import ChallengesTest from './sections/Challenges/ChallengesTest.jsx' 
-import React from 'react'
-import './App.css'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Hero />
-    <Challenges />
-    <Quiz />
-    <ChallengesTest />
-    <ConfessionWall />
-    <ProfileSection />
-
-  </StrictMode>,
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import Hero from "./sections/DASHBOARD/Hero.jsx"
+import Challenges from "./sections/Challenges/Challenges.jsx"
+import ConfessionWall from "./sections/ConfessionWall/ConfessionWall.jsx"
+import ProfileSection from "./sections/ProfileSection/ProfileSection.jsx"
+import Quiz from "./sections/Challenges/quiz.jsx"
+import ChallengesTest from "./sections/Challenges/ChallengesTest.jsx"
+import React from "react"
+import "./App.css"
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <Hero />
+        <Challenges />
+        <Quiz />
+        <ChallengesTest />
+        <ConfessionWall />
+        <ProfileSection />
+    </StrictMode>
 )
