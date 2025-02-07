22011a47ca061e110576070555ce5069c2ac616b
Pratik <pkbhopi132@gmail.com>
2025-02-08 01:57:46 +0530

    match

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { registerUser, loginUser } = require('../controllers/AuthControllers')
+const { registerUser, loginUser } = require('../controllers/AuthControllers');
+const { findMatch } = require('../controllers/FindMatch');
 const UserRouter = express.Router()
 
 
 UserRouter.post('/register',registerUser);
 UserRouter.post('/login',loginUser);
 
+UserRouter.post('/find-match',findMatch);
 module.exports = UserRouter;
